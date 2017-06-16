1d82dd75c40bc2195a05618d6deb125ab669a692
Scott Jackson <scottmmjackson@gmail.com>
2017-06-16 12:55:48 -0400

    force to compile...

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fromPairs = require("lodash.frompairs");
-const ngcomponent_1 = require("ngcomponent");
-const React = require("react");
-const react_dom_1 = require("react-dom");
+var fromPairs = require("lodash.frompairs");
+var ngcomponent_1 = require("ngcomponent");
+var React = require("react");
+var react_dom_1 = require("react-dom");
 /**
  * Wraps a React component in Angular. Returns a new Angular component.
  *
@@ -16,24 +34,27 @@ const react_dom_1 = require("react-dom");
  *   ```
  */
 function react2angular(Class, bindingNames) {
-    const names = bindingNames
+    var names = bindingNames
         || (Class.propTypes && Object.keys(Class.propTypes))
         || [];
     return {
-        bindings: fromPairs(names.map(_ => [_, '<'])),
-        controller: ['$element', class extends ngcomponent_1.default {
-                constructor($element) {
-                    super();
-                    this.$element = $element;
+        bindings: fromPairs(names.map(function (_) { return [_, '<']; })),
+        controller: ['$element', (function (_super) {
+                __extends(class_1, _super);
+                function class_1($element) {
+                    var _this = _super.call(this) || this;
+                    _this.$element = $element;
+                    return _this;
                 }
-                render() {
+                class_1.prototype.render = function () {
                     // TODO: rm any when https://github.com/Microsoft/TypeScript/pull/13288 is merged
-                    react_dom_1.render(React.createElement(Class, Object.assign({}, this.props)), this.$element[0]);
-                }
-                componentWillUnmount() {
+                    react_dom_1.render(React.createElement(Class, __assign({}, this.props)), this.$element[0]);
+                };
+                class_1.prototype.componentWillUnmount = function () {
                     react_dom_1.unmountComponentAtNode(this.$element[0]);
-                }
-            }]
+                };
+                return class_1;
+            }(ngcomponent_1.default))]
     };
 }
 exports.react2angular = react2angular;
--- test.js
+++ test.js
@@ -1,13 +1,28 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-const angular_1 = require("angular");
+var angular_1 = require("angular");
 require("angular-mocks");
-const ngimport_1 = require("ngimport");
-const React = require("react");
-const test_utils_1 = require("react-dom/test-utils");
-const _1 = require("./");
-class TestOne extends React.Component {
-    render() {
+var ngimport_1 = require("ngimport");
+var React = require("react");
+var test_utils_1 = require("react-dom/test-utils");
+var _1 = require("./");
+var TestOne = (function (_super) {
+    __extends(TestOne, _super);
+    function TestOne() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    TestOne.prototype.render = function () {
+        var _this = this;
         return React.createElement("div", null,
             React.createElement("p", null,
                 "Foo: ",
@@ -15,22 +30,30 @@ class TestOne extends React.Component {
             React.createElement("p", null,
                 "Bar: ",
                 this.props.bar.join(',')),
-            React.createElement("p", { onClick: () => this.props.baz(42) }, "Baz"),
+            React.createElement("p", { onClick: function () { return _this.props.baz(42); } }, "Baz"),
             this.props.children);
+    };
+    TestOne.prototype.componentWillUnmount = function () { };
+    return TestOne;
+}(React.Component));
+var TestTwo = function (props) {
+    return React.createElement("div", null,
+        React.createElement("p", null,
+            "Foo: ",
+            props.foo),
+        React.createElement("p", null,
+            "Bar: ",
+            props.bar.join(',')),
+        React.createElement("p", { onClick: function () { return props.baz(42); } }, "Baz"),
+        props.children);
+};
+var TestThreeWithPropTypes = (function (_super) {
+    __extends(TestThreeWithPropTypes, _super);
+    function TestThreeWithPropTypes() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    componentWillUnmount() { }
-}
-const TestTwo = props => React.createElement("div", null,
-    React.createElement("p", null,
-        "Foo: ",
-        props.foo),
-    React.createElement("p", null,
-        "Bar: ",
-        props.bar.join(',')),
-    React.createElement("p", { onClick: () => props.baz(42) }, "Baz"),
-    props.children);
-class TestThreeWithPropTypes extends React.Component {
-    render() {
+    TestThreeWithPropTypes.prototype.render = function () {
+        var _this = this;
         return React.createElement("div", null,
             React.createElement("p", null,
                 "Foo: ",
@@ -38,166 +61,171 @@ class TestThreeWithPropTypes extends React.Component {
             React.createElement("p", null,
                 "Bar: ",
                 this.props.bar.join(',')),
-            React.createElement("p", { onClick: () => this.props.baz(42) }, "Baz"),
+            React.createElement("p", { onClick: function () { return _this.props.baz(42); } }, "Baz"),
             this.props.children);
-    }
-    componentWillUnmount() { }
-}
+    };
+    TestThreeWithPropTypes.prototype.componentWillUnmount = function () { };
+    return TestThreeWithPropTypes;
+}(React.Component));
 TestThreeWithPropTypes.propTypes = {
     bar: React.PropTypes.array.isRequired,
     baz: React.PropTypes.func.isRequired,
     foo: React.PropTypes.number.isRequired
 };
-const TestAngularOne = _1.react2angular(TestOne, ['foo', 'bar', 'baz']);
-const TestAngularTwo = _1.react2angular(TestTwo, ['foo', 'bar', 'baz']);
+var TestAngularOne = _1.react2angular(TestOne, ['foo', 'bar', 'baz']);
+var TestAngularTwo = _1.react2angular(TestTwo, ['foo', 'bar', 'baz']);
 angular_1.module('test', ['bcherny/ngimport'])
     .component('testAngularOne', TestAngularOne)
     .component('testAngularTwo', TestAngularTwo);
 angular_1.bootstrap(angular_1.element(), ['test'], { strictDi: true });
-describe('react2angular', () => {
-    let $compile;
-    beforeEach(() => {
+describe('react2angular', function () {
+    var $compile;
+    beforeEach(function () {
         angular_1.mock.module('test');
         angular_1.mock.inject(function (_$compile_) {
             $compile = _$compile_;
         });
     });
-    describe('initialization', () => {
-        it('should give an angular component', () => {
+    describe('initialization', function () {
+        it('should give an angular component', function () {
             expect(TestAngularOne.bindings).not.toBe(undefined);
             expect(TestAngularOne.controller).not.toBe(undefined);
         });
-        it('should use the propTypes when present and no bindingNames were specified', () => {
-            const reactAngularComponent = _1.react2angular(TestThreeWithPropTypes);
+        it('should use the propTypes when present and no bindingNames were specified', function () {
+            var reactAngularComponent = _1.react2angular(TestThreeWithPropTypes);
             expect(reactAngularComponent.bindings).toEqual({
                 bar: '<',
                 baz: '<',
                 foo: '<'
             });
         });
-        it('should use the bindingNames when present over the propTypes', () => {
-            const reactAngularComponent = _1.react2angular(TestThreeWithPropTypes, ['foo']);
+        it('should use the bindingNames when present over the propTypes', function () {
+            var reactAngularComponent = _1.react2angular(TestThreeWithPropTypes, ['foo']);
             expect(reactAngularComponent.bindings).toEqual({
                 foo: '<'
             });
         });
-        it('should have empty bindings when parameter is an empty array', () => {
-            const reactAngularComponent = _1.react2angular(TestThreeWithPropTypes, []);
+        it('should have empty bindings when parameter is an empty array', function () {
+            var reactAngularComponent = _1.react2angular(TestThreeWithPropTypes, []);
             expect(reactAngularComponent.bindings).toEqual({});
         });
     });
-    describe('react classes', () => {
-        it('should render', () => {
-            const scope = Object.assign(ngimport_1.$rootScope.$new(true), {
+    describe('react classes', function () {
+        it('should render', function () {
+            var scope = Object.assign(ngimport_1.$rootScope.$new(true), {
                 bar: [true, false],
-                baz: (value) => value + 1,
+                baz: function (value) { return value + 1; },
                 foo: 1
             });
-            const element = angular_1.element(`<test-angular-one foo="foo" bar="bar" baz="baz"></test-angular-one>`);
+            var element = angular_1.element("<test-angular-one foo=\"foo\" bar=\"bar\" baz=\"baz\"></test-angular-one>");
             $compile(element)(scope);
             ngimport_1.$rootScope.$apply();
             expect(element.find('p').length).toBe(3);
         });
-        it('should update', () => {
-            const scope = Object.assign(ngimport_1.$rootScope.$new(true), {
+        it('should update', function () {
+            var scope = Object.assign(ngimport_1.$rootScope.$new(true), {
                 bar: [true, false],
-                baz: (value) => value + 1,
+                baz: function (value) { return value + 1; },
                 foo: 1
             });
-            const element = angular_1.element(`<test-angular-one foo="foo" bar="bar" baz="baz"></test-angular-one>`);
+            var element = angular_1.element("<test-angular-one foo=\"foo\" bar=\"bar\" baz=\"baz\"></test-angular-one>");
             $compile(element)(scope);
             ngimport_1.$rootScope.$apply();
             expect(element.find('p').eq(1).text()).toBe('Bar: true,false');
-            scope.$apply(() => scope.bar = [false, true, true]);
+            scope.$apply(function () {
+                return scope.bar = [false, true, true];
+            });
             expect(element.find('p').eq(1).text()).toBe('Bar: false,true,true');
         });
-        it('should destroy', () => {
-            const scope = Object.assign(ngimport_1.$rootScope.$new(true), {
+        it('should destroy', function () {
+            var scope = Object.assign(ngimport_1.$rootScope.$new(true), {
                 bar: [true, false],
-                baz: (value) => value + 1,
+                baz: function (value) { return value + 1; },
                 foo: 1
             });
-            const element = angular_1.element(`<test-angular-one foo="foo" bar="bar" baz="baz"></test-angular-one>`);
+            var element = angular_1.element("<test-angular-one foo=\"foo\" bar=\"bar\" baz=\"baz\"></test-angular-one>");
             $compile(element)(scope);
             ngimport_1.$rootScope.$apply();
             spyOn(TestOne.prototype, 'componentWillUnmount');
             scope.$destroy();
             expect(TestOne.prototype.componentWillUnmount).toHaveBeenCalled();
         });
-        it('should take callbacks', () => {
-            const baz = jasmine.createSpy('baz');
-            const scope = Object.assign(ngimport_1.$rootScope.$new(true), {
+        it('should take callbacks', function () {
+            var baz = jasmine.createSpy('baz');
+            var scope = Object.assign(ngimport_1.$rootScope.$new(true), {
                 bar: [true, false],
-                baz,
+                baz: baz,
                 foo: 1
             });
-            const element = angular_1.element(`<test-angular-one foo="foo" bar="bar" baz="baz"></test-angular-one>`);
+            var element = angular_1.element("<test-angular-one foo=\"foo\" bar=\"bar\" baz=\"baz\"></test-angular-one>");
             $compile(element)(scope);
             ngimport_1.$rootScope.$apply();
             test_utils_1.Simulate.click(element.find('p').eq(2)[0]);
             expect(baz).toHaveBeenCalledWith(42);
         });
         // TODO: support children
-        it('should not support children', () => {
-            const scope = Object.assign(ngimport_1.$rootScope.$new(true), {
+        it('should not support children', function () {
+            var scope = Object.assign(ngimport_1.$rootScope.$new(true), {
                 bar: [true, false],
-                baz: (value) => value + 1,
+                baz: function (value) { return value + 1; },
                 foo: 1
             });
-            const element = angular_1.element(`<test-angular-one foo="foo" bar="bar" baz="baz"><span>Transcluded</span></test-angular-one>`);
+            var element = angular_1.element("<test-angular-one foo=\"foo\" bar=\"bar\" baz=\"baz\"><span>Transcluded</span></test-angular-one>");
             $compile(element)(scope);
             ngimport_1.$rootScope.$apply();
             expect(element.find('span').length).toBe(0);
         });
     });
-    describe('react stateless components', () => {
-        it('should render', () => {
-            const scope = Object.assign(ngimport_1.$rootScope.$new(true), {
+    describe('react stateless components', function () {
+        it('should render', function () {
+            var scope = Object.assign(ngimport_1.$rootScope.$new(true), {
                 bar: [true, false],
-                baz: (value) => value + 1,
+                baz: function (value) { return value + 1; },
                 foo: 1
             });
-            const element = angular_1.element(`<test-angular-two foo="foo" bar="bar" baz="baz"></test-angular-two>`);
+            var element = angular_1.element("<test-angular-two foo=\"foo\" bar=\"bar\" baz=\"baz\"></test-angular-two>");
             $compile(element)(scope);
             ngimport_1.$rootScope.$apply();
             expect(element.find('p').length).toBe(3);
         });
-        it('should update', () => {
-            const scope = Object.assign(ngimport_1.$rootScope.$new(true), {
+        it('should update', function () {
+            var scope = Object.assign(ngimport_1.$rootScope.$new(true), {
                 bar: [true, false],
-                baz: (value) => value + 1,
+                baz: function (value) { return value + 1; },
                 foo: 1
             });
-            const element = angular_1.element(`<test-angular-two foo="foo" bar="bar" baz="baz"></test-angular-two>`);
+            var element = angular_1.element("<test-angular-two foo=\"foo\" bar=\"bar\" baz=\"baz\"></test-angular-two>");
             $compile(element)(scope);
             ngimport_1.$rootScope.$apply();
             expect(element.find('p').eq(1).text()).toBe('Bar: true,false');
-            scope.$apply(() => scope.bar = [false, true, true]);
+            scope.$apply(function () {
+                return scope.bar = [false, true, true];
+            });
             expect(element.find('p').eq(1).text()).toBe('Bar: false,true,true');
         });
         // TODO: figure out how to test this
-        xit('should destroy', () => { });
-        it('should take callbacks', () => {
-            const baz = jasmine.createSpy('baz');
-            const scope = Object.assign(ngimport_1.$rootScope.$new(true), {
+        xit('should destroy', function () { });
+        it('should take callbacks', function () {
+            var baz = jasmine.createSpy('baz');
+            var scope = Object.assign(ngimport_1.$rootScope.$new(true), {
                 bar: [true, false],
-                baz,
+                baz: baz,
                 foo: 1
             });
-            const element = angular_1.element(`<test-angular-two foo="foo" bar="bar" baz="baz"></test-angular-two>`);
+            var element = angular_1.element("<test-angular-two foo=\"foo\" bar=\"bar\" baz=\"baz\"></test-angular-two>");
             $compile(element)(scope);
             ngimport_1.$rootScope.$apply();
             test_utils_1.Simulate.click(element.find('p').eq(2)[0]);
             expect(baz).toHaveBeenCalledWith(42);
         });
         // TODO: support children
-        it('should not support children', () => {
-            const scope = Object.assign(ngimport_1.$rootScope.$new(true), {
+        it('should not support children', function () {
+            var scope = Object.assign(ngimport_1.$rootScope.$new(true), {
                 bar: [true, false],
-                baz: (value) => value + 1,
+                baz: function (value) { return value + 1; },
                 foo: 1
             });
-            const element = angular_1.element(`<test-angular-two foo="foo" bar="bar" baz="baz"><span>Transcluded</span></test-angular-two>`);
+            var element = angular_1.element("<test-angular-two foo=\"foo\" bar=\"bar\" baz=\"baz\"><span>Transcluded</span></test-angular-two>");
             $compile(element)(scope);
             ngimport_1.$rootScope.$apply();
             expect(element.find('span').length).toBe(0);
